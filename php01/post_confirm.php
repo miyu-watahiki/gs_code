3b41a9db15dcd4128fc5c32a5745b9ee2b0f1308
miyu-watahiki <mmm52.kt@gmail.com>
2020-05-24 16:04:12 +0900

    初回課題

--- php01/post_confirm.php
+++ php01/post_confirm.php
@@ -1,15 +1,22 @@
 <?php
+function h($value){
+    return htmlspecialchars($value,ENT_QUOTES);
+}
+
+
 $flg = 0;
-$name = $_POST["name"];
-$mail = $_POST["mail"];
-if($name==""){
-    $name = "未入力です";
-    $flg = 1;
-}
-if($mail==""){
-    $mail = "未入力です";
-    $flg = 1;
-}
+ $name = $_POST["name"];
+ $mail = $_POST["mail"];
+
+// $s = post("$name","$mail"); 
+// echo $s;
+
+//File書き込み
+$file = fopen("./data/data.txt","a");	// ファイル読み込み
+fwrite($file, $name.",".$mail."\r\n");
+fclose($file);
+
+
 ?>
 <html>
 <head>
@@ -17,8 +24,8 @@ if($mail==""){
 <title>POST（受信）</title>
 </head>
 <body>
-お名前：<?php echo $name; ?>
-EMAIL：<?php echo $mail; ?>
+お名前：<?php echo h($name); ?>
+EMAIL：<?php echo h($mail); ?>
 <?php
     if($flg == 0){
     ?>
